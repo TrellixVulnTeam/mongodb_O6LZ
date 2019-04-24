aa85376cf895518ad1f4946a5c2f3ce96787906f
juangui115 <juanpalma122800@correo.itm.edu.co>
2019-04-24 15:55:30 -0500

    refactorizado codigo
    refactorizado y cambio de nombre funciones

--- crud/crud.py
+++ crud/crud.py
@@ -1,237 +1,102 @@
-import pymongo
+import pymongo,json,time,urllib.parse
 from bson.objectid import ObjectId
-import json
-from pymongo.errors import ConnectionFailure
-import urllib.parse
 from pymongo import MongoClient
-import time
 
 class connectionobj(object):
 
-  def __init__(self,userobj,passwobj,uriobj,connectionobj,dbobj,secuobj,intenobj):
-        self.user = userobj
-        self.passw = passwobj
-        self.uri = uriobj
+  def __init__(self,connectionobj,dbobj,secuobj,intenobj):
         self.connection = connectionobj
         self.db = dbobj
         self.secu = secuobj
         self.inten = intenobj
-        self.boole = False
-
-  def connectiontes(self):
-    self.myclientconnection()
+        self.boolconnection = False
   
-  def coleccion(self,col):
-        return self.mydb[col]   
-  
-  def myclientconnection(self):
-    username = urllib.parse.quote_plus('user')
-    username = self.user
-    password = urllib.parse.quote_plus('pass/word')
-    password = self.passw
-    uricon = urllib.parse.quote_plus('uri')
-    uricon = self.uri
+  def my_client_connection(self):
     for i in range(self.inten):
           try:
-            #self.myclient = pymongo.MongoClient(self.connection)
-            self.myclient = pymongo.MongoClient('mongodb+srv://%s:%s@%s' % (username, password, uricon))
+            self.myclient = pymongo.MongoClient(self.connection)
             self.mydb = self.myclient[self.db]
-            self.boole = True
+            self.boolconnection = True
             return True
           except Exception as e:
-            self.boole = False
+            print(e)
+            self.boolconnection = False
             self.error= e
             time.sleep(self.secu)
         
-  def testdbconnection(self):
+  def validate_connection(self):
     for i in range(self.inten):
           try:
             self.mydb.command("serverStatus")
-            self.boole = True
+            self.boolconnection = True
             return True
           except Exception as e:
-            self.boole = False
+            self.boolconnection = False
             self.error = e  
             time.sleep(self.secu)
-        
-  def insert(self,data,col):
-      if(self.boole == False):
-        if (self.myclientconnection() == True):
-            try:
-              return self.mydb[col].insert_one(data)
-            except Exception as e:
-              self.error = e
-              return self.error
+
+  def insert_main(self,data,col):
+      typeobj = self.mydb[col].insert_one
+      datastructure = [data]
+      return self.mongo_called(typeobj,datastructure)
+  
+  def insert_many_main(self,data,col):
+      typeobj = self.mydb[col].insert_many
+      datastructure = [data]
+      return self.mongo_called(typeobj,datastructure)
+
+  def update_main(self,id,data,col):
+      typeobj = self.mydb[col].update_one
+      datastructure = [{'_id': ObjectId(id)},{'$set': data}]
+      if(self.data_exist(typeobj,id) == False):
+            return "Object Not Found"
+      return self.mongo_called(typeobj,datastructure)
+
+  def delete_main(self,id,col):
+      typeobj = self.mydb[col].delete_one
+      datastructure = [{'_id':ObjectId(id)}]
+      if(self.data_exist(typeobj,id) == False):
+            return "Object Not Found"
+      return self.mongo_called(typeobj,datastructure)
+
+
+  def find_main(self,id,col):
+      typeobj = self.mydb[col].find
+      datastructure = {'_id':ObjectId(id)}
+      return self.data_exist(typeobj,id)
+
+  def data_exist(self,typeobj,id):
+      try: 
+        for x in typeobj({'_id':ObjectId(id)}):
+            return x
+        return False
+      except Exception as e:
+        self.error = e
+        return self.error
+  
+  def mongo_query(self,typeobj,datastructure):
+      try:
+        return typeobj(*datastructure)
+      except Exception as e:
+        self.error = e
+        return self.error
+
+  def mongo_called(self,typeobj,datastructure):
+      if self.boolconnection == False:
+        return self.reconnect_client_connection(typeobj,datastructure)
+      else:
+        if self.validate_connection:
+          return self.mongo_query(typeobj,datastructure)
         else:
+          return self.reconnect_client_connection(typeobj,datastructure)
+  
+  def reconnect_client_connection(self,typeobj,datastructure):
+      if self.my_client_connection:
+          return self.mongo_query(typeobj,datastructure)
+      else:
           return self.error
-      else:
-        if(self.testdbconnection() == True):
-          try:
-            return self.mydb[col].insert_one(data)
-          except Exception as e:
-            self.error = e
-            return self.error
-        else:
-          if (self.myclientconnection() == True):
-                try:
-                  return self.mydb[col].insert_one(data)
-                except Exception as e:
-                  self.error = e
-                  return self.error
-          else:
-            return self.error
-              
-  def insertmult(self,data,col):
-      if(self.boole == False):
-            if (self.myclientconnection() == True):
-              try:
-                return self.mydb[col].insert_many(data)
-              except Exception as e:
-                self.error = e
-                return self.error
-            else:
-              return self.error
-      else:
-        if(self.testdbconnection() == True):
-          try:
-            return self.mydb[col].insert_many(data)
-          except Exception as e:
-            self.error = e
-            return self.error
-        else:
-          if (self.myclientconnection() == True):
-                try:
-                  return self.mydb[col].insert_many(data)
-                except Exception as e:
-                  self.error = e
-                  return self.error
-          else:
-            return self.error
-      
 
-  def update(self,id,data,col):
-      if(find(id,col) == False):
-          return "Object Not Found"
-      if(self.boole == False):
-            if (self.myclientconnection() == True):
-              try:
-                return self.mydb[col].update_one({'_id': ObjectId(id)}, {"$set": data})
-              except Exception as e:
-                self.error = e
-                return self.error
-            else:
-              return self.error
-      else:
-        if(self.testdbconnection() == True):
-          try:
-            return self.mydb[col].update_one({'_id': ObjectId(id)}, {"$set": data})
-          except Exception as e:
-            self.error = e
-            return self.error
-        else:
-          if (self.myclientconnection() == True):
-                try:
-                  return self.mydb[col].update_one({'_id': ObjectId(id)}, {"$set": data})
-                except Exception as e:
-                  self.error = e
-                  return self.error
-          else:
-            return self.error
-
-  def remove_data(self,id,col):
-      if(self.boole == False):
-            if (self.myclientconnection() == True):
-              try:
-                if(find(id,col) == False):
-                  return "Object Not Found"
-                return self.mydb[col].delete_one({'_id':ObjectId(id)})
-              except Exception as e:
-                self.error = e
-                return self.error
-            else:
-              return self.error
-      else:
-          if(self.testdbconnection() == True):
-            try:
-              if(find(id,col) == False):
-                return "Object Not Found"
-              return self.mydb[col].delete_one({'_id':ObjectId(id)})
-            except Exception as e:
-              self.error = e
-              return self.error
-          else:
-            if (self.myclientconnection() == True):
-                  try:
-                    if(find(id,col) == False):
-                      return "Object Not Found"
-                    return self.mydb[col].delete_one({'_id':ObjectId(id)})
-                  except Exception as e:
-                    self.error = e
-                    return self.error
-            else:
-              return self.error  
-
-  def find(self,id,col):
-      if(self.boole == False):
-            if (self.myclientconnection() == True):
-              try:
-                for x in self.mydb[col].find({'_id':ObjectId(id)}):
-                  return x
-                return False
-              except Exception as e:
-                self.error = e
-                return self.error
-            else:
-              return self.error
-      else:
-        if(self.testdbconnection() == True):
-          try:
-            for x in self.mydb[col].find({'_id':ObjectId(id)}):
-              return x
-            return False
-          except Exception as e:
-            self.error = e
-            return self.error
-        else:
-          if (self.myclientconnection() == True):
-                try:
-                  for x in self.mydb[col].find({'_id':ObjectId(id)}):
-                    return x
-                  return False
-                except Exception as e:
-                  self.error = e
-                  return self.error
-          else:
-            return self.error
-
-  def findall(self,col):
-      if(self.boole == False):
-            if (self.myclientconnection() == True):
-              try:
-                return self.mydb[col].find()
-              except Exception as e:
-                self.error = e
-                return self.error
-            else:
-              return self.error
-      else:
-          if(self.testdbconnection() == True):
-            try:
-              return self.mydb[col].find()
-            except Exception as e:
-              self.error = e
-              return self.error
-          else:
-            if (self.myclientconnection() == True):
-              try:
-                return self.mydb[col].find()   
-              except Exception as e:
-                self.error = e
-                return self.error
-              else:
-                return self.error
-
-  def close(self):
-    if(self.testdbconnection == True):
+  def close_connection(self):
+    if self.validate_connection:
       self.myclient.close()
       return True
--- crud_python.py
+++ crud_python.py
@@ -2,33 +2,40 @@
 from crud.crud import *
 
 connection = "mongodb+srv://juangui:Juan5157@cluster0-yew86.azure.mongodb.net/test?retryWrites=true"
+connection = "mongodb://52.90.145.195:27017"
 db= "mydatabase"
 collection = "mycollection"
-test = connectionobj("juangui","Juan5157","cluster0-yew86.azure.mongodb.net/test?retryWrites=true",connection,db,1,3)
+test = connectionobj(connection,db,1,3)
 
-ejemplo = test.connectiontes()
+ejemplo = test.my_client_connection()
 print(ejemplo)
 
 mydata = {'Name':"pruebaconclase"}
-ejemplo = test.insert(mydata,collection)
+ejemplo = test.insert_main(mydata,collection)
 print(ejemplo)
 
 mydata = {'Name':"pruebaconclase2"}
-ejemplo = test.insert(mydata,collection)
+ejemplo = test.insert_main(mydata,collection)
 print(ejemplo)
 
 with open('jsontest.json') as template:
         template_dct = json.load(template)
 
-ejemplo = test.insert(template_dct,collection)
+ejemplo = test.insert_main(template_dct,collection)
 
-test.close()
 
-#id = "5cb0e141bc216d25ac165922"
-#mydataupd = { 'name': "Carlitos tevez", 'address': "Apple st 652"}
-#ejemplo = crud.crud.find('5cb0bd8bbc216d283c871b20')
-#ejemplo = crud.crud.remove_data(id)
+
+id = "5cc0cc21bc216d4a38d8f188"
+mydataupd = {'name': "Carlitos tevez", 'address': "Apple st 652"}
+ejemplo = test.update_main(id,mydataupd,collection)
+print(ejemplo)
+ejemplo = test.find_main(id,collection)
+print(ejemplo)
+#ejemplo = test.delete_main(id,collection)
 #print(ejemplo)
+
+test.close_connection()
+
 ''' mylist = [
   { "name": "Amy", "address": "Apple st 652"},
   { "name": "Hannah", "address": "Mountain 21"},
